a69a6b995bc76ee22bb19080add3c6427aee0377
Jake <haekbreen@gmail.com>
2019-08-20 23:05:21 -0700

    Update some settings to allow debugging of Object.lto.

--- NOLF/ClientShellDLL/_after_build.bat
+++ NOLF/ClientShellDLL/_after_build.bat
@@ -1,4 +1,5 @@
 copy CShell.dll "D:\Games\NOLF - dev\DebugDLL\"
+copy ..\ObjectDLL\Object.lto "D:\Games\NOLF - dev\DebugDLL\"
 cd "D:\Games\NOLF - dev\Tools"
 .\lithrez.exe c ..\DebugDLL.rez ..\DebugDLL
 echo "Finished!"
