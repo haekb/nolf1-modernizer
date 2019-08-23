3b14d20b596c45d624e3774bf750bd3533406f9a
Jake <haekbreen@gmail.com>
2019-08-22 23:35:19 -0700

    Over-verbosely figure out some 0 on 0 division.

--- NOLF/ObjectDLL/AIPathMgr.cpp
+++ NOLF/ObjectDLL/AIPathMgr.cpp
@@ -385,7 +385,27 @@ void CAIPathMgr::BuildPath(CAI* pAI, CAIPath* pPath, CAIVolume* pVolume, const L
             LTVector vv = pVolumeNeighbor->GetConnectionPerpDir();
             LTVector vP = pVolumeNeighbor->GetConnectionPos();
 
+#if 1
+			// Breakdown in hopes to figure out nan!
+			LTVector volumePositionMinusWalkthroughPosition = vP - vQ;
+			float positionDotProduct = volumePositionMinusWalkthroughPosition.Dot(vv);
+			float walkthroughPositionDotProduct = vW.Dot(vv);
+			LTVector walkthroughPoisitonsAdded = vQ + vW;
+
+			// 0 divides by 0 in some cases...
+			float twoDotProductsDivided = positionDotProduct / walkthroughPositionDotProduct;
+
+			// Reset twoDotProductsDivded
+			if (isnan(twoDotProductsDivided))
+			{
+				// Either 1.0 or 0.0. Try to mimic how VC++ 6.0's -1.#IND casts.
+				twoDotProductsDivided = 1.0f;
+			}
+
+			vIntersectionPoint = walkthroughPoisitonsAdded * twoDotProductsDivided;
+#else
 			vIntersectionPoint = vQ + vW*((vP-vQ).Dot(vv)/(vW.Dot(vv)));
+#endif
 		}
 
 		// Clip it to the connection line segment
