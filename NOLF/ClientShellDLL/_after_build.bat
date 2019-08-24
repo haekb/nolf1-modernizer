8da6b93cf1a2ba3dfe9bd8fb7e725355d26b30ab
Jake <haekbreen@gmail.com>
2019-08-23 19:59:52 -0700

    Cleaned up project. Debug and Final Release are now buildable.

--- NOLF/ClientShellDLL/_after_build.bat
+++
@@ -1,5 +0,0 @@
-copy CShell.dll "D:\Games\NOLF - dev\DebugDLL\"
-copy ..\ObjectDLL\Object.lto "D:\Games\NOLF - dev\DebugDLL\"
-cd "D:\Games\NOLF - dev\Tools"
-.\lithrez.exe c ..\DebugDLL.rez ..\DebugDLL
-echo "Finished!"
---
+++ NOLF/_after_build_debug.bat
@@ -0,0 +1,3 @@
+copy .\ClientShellDLL\CShell.dll "D:\Games\NOLF - dev\DebugDLL\"
+copy .\ObjectDLL\Object.lto "D:\Games\NOLF - dev\DebugDLL\"
+echo "Finished!"
