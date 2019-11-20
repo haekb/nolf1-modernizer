910d12065a0a22781fe9cd05e8f6a8a9793094ea
Jake <haekbreen@gmail.com>
2019-11-19 20:56:34 -0800

    Found the really correct ConsolePrint engine functions.

--- NOLF/ClientShellDLL/DetourMgr.cpp
+++ NOLF/ClientShellDLL/DetourMgr.cpp
@@ -11,21 +11,27 @@ DetourMgr::DetourMgr()
 {
 	g_pDetourMgr = this;
 	m_pConsoleFunc = NULL;
+	m_pConsoleFuncVa = NULL;
+	m_pConsoleFuncSimple = NULL;
 }
 
 DetourMgr::~DetourMgr()
 {
+	Term();
 }
 
 void DetourMgr::Init()
 {
 	m_pConsoleFunc = (int*)CONSOLE_FUNCTION_PTR;
+	m_pConsoleFuncVa = (int*)CONSOLE_FUNCTION_PTR_VA;
+	m_pConsoleFuncSimple = (int*)CONSOLE_FUNCTION_PTR_SIMPLE;
 
 	DetourTransactionBegin();
 	DetourUpdateThread(GetCurrentThread());
 
 	// Attach some functions!
 	DetourAttach(&(PVOID&)m_pConsoleFunc, df_Console);
+	DetourAttach(&(PVOID&)m_pConsoleFuncSimple, df_ConsoleSimple);
 
 	DetourTransactionCommit();
 
@@ -36,23 +42,24 @@ void DetourMgr::Term()
 	DetourTransactionBegin();
 	DetourUpdateThread(GetCurrentThread());
 	DetourDetach(&(PVOID&)m_pConsoleFunc, df_Console);
+	DetourDetach(&(PVOID&)m_pConsoleFuncSimple, df_ConsoleSimple);
 	DetourTransactionCommit();
 }
 
-
-void DetourMgr::ConsolePrint(int iColour, int iLevel, const char* pFormat, va_list vaList)
+// Main console print method
+// We can filter with iLevel, although I've only seen 0 and 1 be used, and I'm not sure either's context.
+// Colour works, so that's neat.
+void DetourMgr::ConsolePrint(HLTCOLOR iColour, int iLevel, const char* pMsg)
 {
 	if (!g_pConsoleMgr) {
 		return;
 	}
 
-	char aMsgBuffer[2048];
-	memset(aMsgBuffer, '\0', sizeof(aMsgBuffer));
-	_vsnprintf(aMsgBuffer, sizeof(aMsgBuffer), pFormat, vaList);
+	std::string sString = "[" + std::to_string(iLevel) + "] " + pMsg;
 
-	aMsgBuffer[sizeof(aMsgBuffer) - 1] = '\0';
+	g_pConsoleMgr->Read(sString.c_str(), iColour, iLevel);
 
-	g_pConsoleMgr->Read(aMsgBuffer, iColour, iLevel);
-
-	SDL_Log("CONSOLE: %s", aMsgBuffer);
+	// Run the original console print
+	((ConsoleFn)m_pConsoleFunc)(iColour, iLevel, pMsg);
 }
+
--- NOLF/ClientShellDLL/DetourMgr.h
+++ NOLF/ClientShellDLL/DetourMgr.h
@@ -2,9 +2,17 @@
 
 // Debug Output?
 //#define CONSOLE_FUNCTION_PTR 0x496B1B;
+// Formatting?
+//#define CONSOLE_FUNCTION_PTR 0x49318E;
+// Not it
+//#define CONSOLE_FUNCTION_PTR 0x41d1c0;
 
-#define CONSOLE_FUNCTION_PTR 0x49318E;
+// Console Print
+#define CONSOLE_FUNCTION_PTR 0x41b880;
+#define CONSOLE_FUNCTION_PTR_SIMPLE 0x41b8c0;
+#define CONSOLE_FUNCTION_PTR_VA 0x41d380;
 
+typedef void (*ConsoleFn)(HLTCOLOR iColour, int iLevel, const char* pMsg);
 
 class DetourMgr
 {
@@ -15,17 +23,46 @@ public:
 	void Init();
 	void Term();
 
-	void ConsolePrint(int iColour, int iLevel, const char* pFormat, va_list vaList);
+	void ConsolePrint(HLTCOLOR iColour, int iLevel, const char* pMsg);
 
 protected:
 
 private:
 
 	int* m_pConsoleFunc;
+	int* m_pConsoleFuncVa;
+	int* m_pConsoleFuncSimple;
 
 };
 
 extern DetourMgr* g_pDetourMgr;
 
 // Detour Functions
-inline void df_Console(int iColour, int iLevel, const char* pFormat, va_list vaList) { if (!g_pDetourMgr) return; g_pDetourMgr->ConsolePrint(iColour, iLevel, pFormat, vaList); };
+inline void df_Console(HLTCOLOR iColour, int iLevel, const char* pMsg) { if (!g_pDetourMgr) return; g_pDetourMgr->ConsolePrint(iColour, iLevel, pMsg); };
+
+// The original function, there's only one reference that calls this, so I'm not sure it's correct...
+// So it's not used!
+inline void df_ConsoleVa(HLTCOLOR iColour, int iLevel, const char* pFormat, va_list vaList)
+{
+	if (!g_pConsoleMgr) {
+		return;
+	}
+
+	char aMsgBuffer[2048];
+	memset(aMsgBuffer, '\0', sizeof(aMsgBuffer));
+	_vsnprintf(aMsgBuffer, sizeof(aMsgBuffer), pFormat, vaList);
+
+	aMsgBuffer[strlen(aMsgBuffer)] = '\0';
+
+	df_Console(iColour, iLevel, aMsgBuffer);
+}
+
+// Someone just wants to print a message, that's cool.
+inline void df_ConsoleSimple(const char* pMsg)
+{
+	if (!g_pConsoleMgr) {
+		return;
+	}
+
+	df_Console(SETRGB(255,255,255), 0, pMsg);
+}
